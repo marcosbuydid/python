96e498f79e175e7483f8968a686d46e0d0af01c6
marcos <marcosbuydid@gmail.com>
2025-07-22 23:32:02 -0300

    add set file

---
+++ set/basics.py
@@ -0,0 +1,14 @@
+# unordered collection of elements
+# all elements are distinct
+# elements cannot be accessed by index
+S = {1, 40, 23, 33, 98, 3}
+S1 = {1, 2, 9 + 5j, 'Clari', True}
+S2 = set([1, 2, 3, 4])
+S3 = set('Kaggle')
+S4 = {9}
+
+# position of the elements in a set
+# is not predictable
+for x in S:
+    print(x, end=' ')
+
